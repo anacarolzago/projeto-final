d8ba417ea354966bde5a869bc1bf83cf8dc93739
Fernando Souza <carolina.anazago@gmail.com>
2021-12-18 05:47:52 +0000

    projeto finalizado

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
         <div className='rodape'>
             <footer>
                 <div className='hide'></div>
+
                 <div>
                     <h3><span>Kombucha</span>Lover's</h3>
                 </div>
-
                 <div className='links_footer'>
                     <h4>links</h4>
                     <ul>
@@ -19,20 +19,16 @@ const Footer = () => {
                         <li><Link className="link_footer" to="/perguntas">perguntas frequentes</Link></li>
                     </ul>
                 </div>
-
-                <div className='links_footer'>
-                    <h4>contato</h4>
+                <div className='links_footer_contato'>
                     <ul>
-                        <li><Link className="link_footer" to="/">home</Link></li>
-                        <li><Link className="link_footer" to="/beneficios">benefícios</Link></li>
-                        <li><Link className="link_footer" to="/doar">quero doação / quero doar</Link></li>
-                        <li><Link className="link_footer" to="/perguntas">perguntas frequentes</Link></li>
+                        <li><Link className="link_footer" to="/">whatsapp</Link></li>
+                        <li><Link className="link_footer" to="/">instagram</Link></li>
+                        <li><Link className="link_footer" to="/">facebook</Link></li>
                     </ul>
                 </div>
 
                 <div className='hide'></div>
             </footer>
-
             <div className='rodape_p'>
                 <p>Projeto realizado pela aluna Ana Carolina Zago - Reprograma &copy; 2021</p>
             </div>
