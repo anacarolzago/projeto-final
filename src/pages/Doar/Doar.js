746d6cf58b7389b25d445c7a1af316f901c5ee24
Fernando Souza <carolina.anazago@gmail.com>
2021-12-18 05:36:34 +0000

    página doar finalizada com o form

--- src/pages/Doar/Doar.js
+++ src/pages/Doar/Doar.js
@@ -6,6 +6,8 @@ import Main from '../../components/Main/Main'
 import Footer from '../../components/Footer/Footer'
 
 import './doar.styles.css'
+import Image8 from '../../assets/localizacao.svg'
+
 
 const Doar = () => {
     const [nome, setNome] = useState("")
@@ -19,93 +21,97 @@ const Doar = () => {
             <Header title='Kombucha' />
             <Main> 
                 <div className='container'>
-                    <h1>Cadastro de doares e adotantes</h1>
+                    <article>
+                        <h2>Cadastro de doares e adotantes</h2>
+                        <p>Pra você que já é kombucheira e têm scobys filhos precisando de um novo lar, e pra você que quer fazer suas próprias kombuchas em casa e testar vários sabores mas ainda não tem o scoby e o chá de arranque, preencha nosso fomulário que vamos ver se dá macth!</p>
+                    </article>
+                    <img className='img_form' src={Image8} alt="imagem de kombucha" /> 
+                    <article>
+                        <h3>Como funciona:</h3>
+                        <p>Através do cadastro abaixo, voluntários doadores e pessoas que querem receber doação, colocam seus dados, bem como a cidade, e o nosso sistema vai ver se dá match. Se rolar a combinação de cidades, as duas pessoas receberão um e-mail de confirmação se podem aceitar naquele momento. Após isso enviarem outro e-mail com as informações de cada pessoa, adotante ou do doador, e as duas decidem qual a melhor forma de fazer a adoção.</p>
+                    </article> 
+                    <div className='form'>
+                        <form>
+                            <fieldset className='grupo'>
+                                <div className='campo'>
+                                    <label for="nome"><strong>Nome</strong></label>                        
+                                    <input type="text" nome="nome" id="nome" required value={nome} 
+                                    onChange={(event) => {
+                                        setNome(event.target.value)
+                                    }
+                                    } />
+                                </div>
 
-                    <p>Pra você que já é kombucheira e têm scobys filhos precisando de um novo lar, e pra você que quer fazer suas próprias kombuchas em casa e testar vários sabores mas ainda não tem o scoby e o chá de arranque, preencha nosso fomulário que vamos ver se dá macth!</p>
+                                <div className='campo'>
+                                    <label for="sobrenome"><strong>Sobrenome</strong></label>
+                                    <input type="text" nome="sobrenome" id="sobrenome" required value={sobrenome} 
+                                    onChange={(event) => {
+                                        setSobrenome(event.target.value)
+                                    }
+                                    } />
+                                </div>
+                            </fieldset>
 
-                    <p>Como funciona:</p>
-                    <p>Através do cadastro abaixo, voluntários doadores e pessoas que querem receber doação, colocam seus dados, bem como a cidade, e o nosso sistema vai ver se dá match. Se rolar a combinação de cidades, as duas pessoas receberão um e-mail de confirmação se podem aceitar naquele momento. Após isso enviarem outro e-mail com as informações de cada pessoa, adotante ou do doador, e as duas decidem qual a melhor forma de fazer a adoção.</p>
-
-                    <form>
-                        <fieldset className='grupo'>
                             <div className='campo'>
-                                <label for="nome"><strong>Nome</strong></label>                        
-                                <input type="text" nome="nome" id="nome" required value={nome} 
+                                <label for="email"><strong>Email</strong></label>
+                                <input type="email" nome="email" id="email" required value={email}
                                 onChange={(event) => {
-                                    setNome(event.target.value)
+                                    setEmail(event.target.value)
                                 }
                                 } />
                             </div>
 
                             <div className='campo'>
-                                <label for="sobrenome"><strong>Sobrenome</strong></label>
-                                <input type="text" nome="sobrenome" id="sobrenome" required value={sobrenome} 
+                                <label><strong>Selecione o Estado</strong></label>
+                                <select id="estado" value={estado} 
                                 onChange={(event) => {
-                                    setSobrenome(event.target.value)
+                                    setEstado(event.target.value)
                                 }
-                                } />
+                                } >
+                                    <option selected disabled value="">Selecione</option>
+                                    <option value={"AC"}>AC</option>
+                                    <option value={"AL"}>AL</option>
+                                    <option>AP</option>
+                                    <option>AM</option>
+                                    <option>BA</option>
+                                    <option>CE</option>
+                                    <option>ES</option>
+                                    <option>GO</option>
+                                    <option>MA</option>
+                                    <option>MT</option>
+                                    <option>MS</option>
+                                    <option>MG</option>
+                                    <option>PA</option>
+                                    <option>PB</option>
+                                    <option>PR</option>
+                                    <option>PE</option>
+                                    <option>PI</option>
+                                    <option>RJ</option>
+                                    <option>RN</option>
+                                    <option>RS</option>
+                                    <option>RO</option>
+                                    <option>RR</option>
+                                    <option>SC</option>
+                                    <option>SP</option>
+                                    <option>SE</option>
+                                    <option>TO</option>
+                                    <option>DF</option>
+                                </select>
                             </div>
-                        </fieldset>
 
-                        <div className='campo'>
-                            <label for="email"><strong>Email</strong></label>
-                            <input type="email" nome="email" id="email" required value={email}
-                            onChange={(event) => {
-                                setEmail(event.target.value)
-                            }
-                            } />
-                        </div>
-
-                        <div className='campo'>
-                            <label><strong>Selecione o Estado</strong></label>
-                            <select id="estado" value={estado} 
-                            onChange={(event) => {
-                                setEstado(event.target.value)
-                            }
-                            } >
-                                <option selected disabled value="">Selecione</option>
-                                <option value={"AC"}>AC</option>
-                                <option value={"AL"}>AL</option>
-                                <option>AP</option>
-                                <option>AM</option>
-                                <option>BA</option>
-                                <option>CE</option>
-                                <option>ES</option>
-                                <option>GO</option>
-                                <option>MA</option>
-                                <option>MT</option>
-                                <option>MS</option>
-                                <option>MG</option>
-                                <option>PA</option>
-                                <option>PB</option>
-                                <option>PR</option>
-                                <option>PE</option>
-                                <option>PI</option>
-                                <option>RJ</option>
-                                <option>RN</option>
-                                <option>RS</option>
-                                <option>RO</option>
-                                <option>RR</option>
-                                <option>SC</option>
-                                <option>SP</option>
-                                <option>SE</option>
-                                <option>TO</option>
-                                <option>DF</option>
-                            </select>
-                        </div>
-
-                        <div className='campo'>
-                            <label><strong>Escreva o nome da sua cidade</strong></label>
-                            <textarea row="1" id="cidade" nome="cidade" ></textarea>
-                        </div>
-
-                        <div className='campo'>
-                            <label><strong>Fale conosco</strong></label>
-                            <textarea row="6" id="experiencia" nome="experiencia"></textarea>
-                        </div>
-
-                        <button type="submit">Concluido</button>
-                    </form>
+                            <div className='campo'>
+                                <label><strong>Escreva o nome da sua cidade</strong></label>
+                                <textarea row="1" id="cidade" nome="cidade" ></textarea>
+                            </div>
+                            <div className='campo'>
+                                <label><strong>Fale conosco</strong></label>
+                                <textarea row="6" id="experiencia" nome="experiencia"></textarea>
+                            </div>
+                            <div>
+                                <button className='botao' type="submit">Concluido</button>
+                            </div>
+                        </form>
+                    </div>         
                 </div>
             </Main>
             <Footer />
